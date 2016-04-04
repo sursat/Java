fc19d785651bfc6360fb09b233ed4abbda441541
sursat <mr.prasadkovvuri@gmail.com>
2016-04-04 17:57:51 +0530

    hello world

---
+++ Java/src/com/begin/basics/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.begin.basics;
+// I know it's a very old way of starting but thing's ahead of this gonna make you exicited :) 
+public class HelloWorld {
+public static void main(String[] args) {
+	System.out.println("Hello World!");
+}
+}
